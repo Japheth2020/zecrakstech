1af9ba629bd71a132d0b44b2e3f2b60f4926403d
japheth-dew <solaadewumi2002@gmail.com>
2022-12-29 14:52:42 +0100

    chnaged origin

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://zecrakstech-hs0g9cp5q-japheth2020.vercel.app'
+export const server = dev ? 'http://localhost:3000' : 'https://zecrakstech.vercel.app/'
